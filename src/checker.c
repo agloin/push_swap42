7b2d9d67eced9aa6fcfecd294dbecc81f80d3a8a
agloin <darabeev@gmail.com>
2020-08-30 23:38:47 +0300

    k

--- src/checker.c
+++ src/checker.c
@@ -38,7 +38,12 @@ int    is_digit(char **argv)
 
 int     is_repeat(char **argv)
 {
-    if (is_digit(argv) == 1) // if there are all numbers, in argv
+    if (is_digit(argv) != 1)
+    {
+        write(1, "Error\n", 6);
+        return (1);
+    }
+    else if (is_digit(argv) == 1) // if there are all numbers, in argv
     {
         int i;
         int j;
@@ -49,12 +54,11 @@ int     is_repeat(char **argv)
             j = i + 1;
             while (argv[j])
             {
-                if (argv[i] == argv[j])
+                if (ft_atoi(argv[i]) == ft_atoi(argv[j]))
                 {
                     write(1, "Error\n", 6);
                     return (1);
                 }
-                printf("argv[i] = %s\nargv[j] = %s\n", argv[i], argv[j]);
                 j++;
             }
             i++;
@@ -88,11 +92,11 @@ int main(int argc, char **argv)
     i = 1;
     if (argc > 1)
     {
-        printf("is_repeat = %d\nis_digit = %d\n", is_repeat(argv), is_digit(argv));
+//        printf("is_repeat = %d\nis_digit = %d\n", is_repeat(argv), is_digit(argv));
         t_stack *tmp;
 
         tmp = ft_lst_new(argv);
-        if (is_digit(argv) == 1 && is_repeat(argv) == 0)
+        if (is_repeat(argv) == 0)
         {
             while (tmp)
             {
